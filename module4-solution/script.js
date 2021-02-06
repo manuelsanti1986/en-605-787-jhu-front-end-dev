8816995984a8a1b7f638e6a60683ccffd8d5be1e
Manuel Santiago <msanti16@jhu.edu>
2021-02-05 19:55:04 -0500

    04: Declared script.js type as module. Finished creating all js files as instructed in comments.

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,7 +1,3 @@
-// *******************************
-// START HERE IF YOU WANT A MORE CHALLENGING STARTING POINT FOR THIS ASSIGNMENT
-// *******************************
-//
 // Module 4 Assignment Instructions.
 //
 // The idea of this assignment is to take an existing array of names
@@ -20,39 +16,19 @@ Hello Larry
 Hello Paula
 Hello Laura
 Good Bye Jim
-
 */
 
-// STEP 1:
-// Wrap the entire contents of script.js inside of an IIFE
-// See Lecture 52, part 2
-// (Note, Step 2 will be done in the SpeakHello.js file.)
+import helloSpeaker from './SpeakHello.js';
+import byeSpeaker from './SpeakGoodBye.js';
 
-let names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+let sayHello = () => {
+  let names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  names.forEach(name => {
+    let firstChar = name.charAt(0).toLowerCase();
+    // console.log(firstChar)
+    (firstChar === 'j')? byeSpeaker.speak(name) : helloSpeaker.speak(name);
+  });
 
-// STEP 10:
-// Loop over the names array and say either 'Hello' or "Good Bye"
-// using either the helloSpeaker's or byeSpeaker's 'speak' method.
-// See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
-
-  // STEP 11:
-  // Retrieve the first letter of the current name in the loop.
-  // Use the string object's 'charAt' function. Since we are looking for
-  // names that start with either upper case or lower case 'J'/'j', call
-  // string object's 'toLowerCase' method on the result so we can compare
-  // to lower case character 'j' afterwards.
-  // Look up these methods on Mozilla Developer Network web site if needed.
-  // var firstLetter =
-
-  // STEP 12:
-  // Compare the 'firstLetter' retrieved in STEP 11 to lower case
-  // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
-  // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
-  // name in the loop.
-  if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
-  } else {
-    // helloSpeaker.xxxx
-  }
 }
+
+sayHello();
