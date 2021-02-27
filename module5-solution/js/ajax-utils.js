2c69a912322a3a87ac5b57dac10e65e5d88d00f1
Manuel Santiago <msanti16@jhu.edu>
2021-02-27 16:55:29 -0500

    05: Added json to response and responseHandler

--- module5-solution/js/ajax-utils.js
+++ module5-solution/js/ajax-utils.js
@@ -12,16 +12,31 @@
         }
     }
 
-    ajaxUtils.sendGetRequest = (requestUrl, responseHandler) => {
-            let request = getRequestObject();
-            request.onreadystatechange = () => handleResponse(request, responseHandler);
-            request.open("GET", requestUrl, true);
-            request.send(null); // for POST only
-        };
+    ajaxUtils.sendGetRequest = (requestUrl, responseHandler, isJsonResponse) => {
+        let request = getRequestObject();
+        request.onreadystatechange = () => handleResponse(request, responseHandler, isJsonResponse);
+        request.open("GET", requestUrl, true);
+        request.send(null); // for POST only
+    };
 
-    let handleResponse = (request, responseHandler) => {
-        if((request.readyState == 4) && (request.status == 200)) {
-            responseHandler(request);
+    ajaxUtils.sendPostRequest = (requestUrl, responseHandler) => {
+        let request = getRequestObject();
+        request.onreadystatechange = () => handleResponse(request, responseHandler);
+        request.open("GET", requestUrl, true);
+        request.send(null); // for POST only
+    };
+
+    let handleResponse = (request, responseHandler, isJsonResponse) => {
+        if((request.readyState === 4) && (request.status === 200)) {
+            if(isJsonResponse === undefined) {
+                isJsonResponse = true;
+            }
+
+            if(isJsonResponse) {
+                responseHandler(JSON.parse(request.responseText));
+            }else {
+                responseHandler(request);
+            }
         }
     }
 
