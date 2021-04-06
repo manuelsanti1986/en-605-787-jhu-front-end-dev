9d2dc0e7540f8c1bc69721c79c70c9a7dee91230
Manuel Santiago <manuelsanti86@gmail.com>
2021-04-05 20:00:11 -0400

    Assignment 08 (#13)
    * 08: Started assignment 8
    
    * 08: Updated index.md
    
    * 08: Copied from assignment3 starter code
    
    * 08: Changed function, controllers, and services names to match the required ones.
    
    * Returning filtered results correctly
    
    * Cleaned up code.
    
    * Addedd factory and removeitem
    
    * Fixed removeitem
    
    * Binding controller and items appearing
    
    * WOrking
    
    * Binding the input from the form to the search
    
    * Added list with remove button and everything is working
    
    * Added tooltip with message.
    
    * Finished
    
    * Finished
    
    * Finished
    
    Co-authored-by: Manuel Santiago <msanti16@jhu.edu>

---
+++ mod3_solution/app.js
@@ -0,0 +1,131 @@
+(function () {
+    'use strict';
+
+    let app = angular.module('NarrowItDownApp', []);
+    app.controller('NarrowItDownController', NarrowItDownController);
+    app.factory('MenuItemsFactory', MenuItemsFactory)
+    app.directive('foundItems', FoundItemsDirective);
+    app.constant('ApiBasePath', 'https://davids-restaurant.herokuapp.com');
+
+    function FoundItemsDirective() {
+        var ddo = {
+            templateUrl: 'menuItems.html',
+            scope: {
+                items: '<',
+                isEmptySearch: '<',
+                onRemove: '&'
+            },
+            controller: NarrowItDownDirectiveController,
+            controllerAs: 'narrowItDown',
+            bindToController: true,
+            link: NarrowItDownDirectiveLink,
+            transclude: true
+        };
+        return ddo;
+    }
+
+    function NarrowItDownDirectiveLink(scope, element, attrs, controller) {
+        scope.$watch('narrowItDown.isEmptyResponse()', function (newValue, oldValue) {
+            if (newValue) {
+                displayEmptySearchWarning();
+            }
+            else {
+                removeEmptySearchWarning();
+            }
+        });
+
+        function displayEmptySearchWarning() {
+            let warningElem = element.find("div.error");
+            warningElem.show();
+        }
+
+        function removeEmptySearchWarning() {
+            let warningElem = element.find('div.error');
+            warningElem.hide();
+        }
+    }
+
+    function NarrowItDownDirectiveController() {
+        let narrowItDown = this;
+
+        narrowItDown.isEmptyResponse = function () {
+            return narrowItDown.isEmptySearch;
+        };
+    }
+
+    NarrowItDownController.$inject = ['MenuItemsFactory'];
+    function NarrowItDownController(MenuItemsFactory) {
+        let narrowItDown = this;
+        let menuItemsFactory = MenuItemsFactory();
+
+        narrowItDown.searchTerm = "";
+        narrowItDown.found = menuItemsFactory.getMenuItems();
+        narrowItDown.isEmptySearch = menuItemsFactory.getIsEmptySearch();
+
+        narrowItDown.getFilteredItems = function(){
+            let filteredItemsPromise = menuItemsFactory.getMatchedMenuItems(narrowItDown.searchTerm);
+            filteredItemsPromise
+                .then(function (){
+                    narrowItDown.found = menuItemsFactory.getMenuItems();
+                    narrowItDown.isEmptySearch = menuItemsFactory.getIsEmptySearch();
+                })
+                .catch(function (error){
+                    console.log("Error: " + error)
+                });
+        };
+
+        narrowItDown.removeMenuItem = function (itemIndex) {
+            menuItemsFactory.removeMenuItem(itemIndex);
+            narrowItDown.found = menuItemsFactory.getMenuItems();
+        };
+
+    };
+
+    function MenuSearchService($http, ApiBasePath){
+        let service = this;
+        let filteredItems = [];
+        let isEmptyResponse = false;
+
+        service.getMatchedMenuItems = function (searchTerm) {
+            let response = $http({
+                method: 'GET',
+                url: (ApiBasePath + '/menu_items.json')
+            }).then(function (response) {
+                filteredItems = response.data.menu_items.filter(function (items) {
+                    return items.description.indexOf(searchTerm) !== -1;
+                });
+                isEmptyResponse = (filteredItems.length === 0)? true : false;
+                return filteredItems;
+            })
+            .catch(function (err) {
+                return `Error: Unable to get items. ${err}`
+            });
+            return response;
+        };
+
+        service.removeMenuItem = function (itemIndex) {
+            if(filteredItems.length > 0) {
+                filteredItems.splice(itemIndex, 1);
+            }
+            return filteredItems;
+        };
+
+        service.getMenuItems = function () {
+            return filteredItems;
+        };
+
+        service.getIsEmptySearch = function () {
+            return isEmptyResponse;
+        };
+    }
+
+    MenuItemsFactory.$inject = ['$http', 'ApiBasePath'];
+    function MenuItemsFactory($http, ApiBasePath) {
+        var factory = function () {
+            return new MenuSearchService($http, ApiBasePath);
+        };
+        return factory;
+    }
+
+})();
+
