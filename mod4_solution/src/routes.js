42f829fe77dc6d2f1b88cd6b276c5527b0d24210
Manuel Santiago <msanti16@jhu.edu>
2021-04-08 19:58:43 -0400

    Created main templates, added the spinner, and the routes

--- mod4_solution/src/routes.js
+++ mod4_solution/src/routes.js
@@ -6,19 +6,27 @@
 
     RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
     function RoutesConfig($stateProvider, $urlRouterProvider) {
-
         $urlRouterProvider.otherwise('/');
-        // *** Set up UI states ***
         $stateProvider
             .state('home', {
                 url: '/',
-                templateUrl: 'src/shoppinglist/templates/home.template.html'
+                templateUrl: 'src/restaurantmenu/templates/home.template.html'
             })
-            .state('mainList', {
-                url: '/main-list',
-                templateUrl: 'src/shoppinglist/templates/main-shoppinglist.template.html',
-                controller: 'MainShoppingListController as mainList'
+
+            .state('categories', {
+                url: '/categories',
+                templateUrl: 'src/restaurantmenu/templates/main-shoppinglist.template.html',
+                controller: 'MainShoppingListController as mainList',
+                resolve: {
+                    items: ['ShoppingListService', function (ShoppingListService) {
+                        return ShoppingListService.getItems();
+                    }]
+                }
+            })
+            .state('mainList.itemDetail', {
+                url: '/item-detail/{itemId}',
+                templateUrl: 'src/restaurantmenu/templates/item-detail.template.html',
+                controller: "ItemDetailController as itemDetail"
             });
     }
-
 })();
