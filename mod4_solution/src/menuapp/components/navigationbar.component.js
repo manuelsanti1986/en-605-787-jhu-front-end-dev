8a41c25c13ac2d690b9ec8f572a5d7005f08257f
Manuel Santiago <msanti16@jhu.edu>
2021-04-09 15:52:59 -0400

    Added navigation bar and home

---
+++ mod4_solution/src/menuapp/components/navigationbar.component.js
@@ -0,0 +1,8 @@
+(function () {
+    'use strict';
+
+    let app = angular.module('MenuApp');
+    app.component('navigationBar', {
+        templateUrl: 'src/menuapp/templates/navigation-bar.template.html',
+    });
+})();
