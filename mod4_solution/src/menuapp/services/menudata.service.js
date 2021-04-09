9431387a587c291d7593495710e32c7b373118ba
Manuel Santiago <msanti16@jhu.edu>
2021-04-08 20:40:42 -0400

    Saving staty in menudata.service.js

--- mod4_solution/src/menuapp/services/menudata.service.js
+++ mod4_solution/src/menuapp/services/menudata.service.js
@@ -8,33 +8,16 @@
     MenuDataService.$inject = ['$q', '$http', 'ApiBasePath'];
     function MenuDataService($q, $http, ApiBasePath) {
         let service = this;
+        let categories = [];
         let items = [];
 
-        // Pre-populate a no cookie list
-        items.push({
-            name: "Sugar",
-            quantity: "2 bags",
-            description: "Sugar used for baking delicious umm... baked goods."
-        });
-        items.push({
-            name: "flour",
-            quantity: "1 bags",
-            description: "High quality wheat flour. Mix it with water, sugar, 2 raw eggs."
-        });
-        items.push({
-            name: "Chocolate Chips",
-            quantity: "3 bags",
-            description: "Put these in the dough. No reason, really. Gotta store them somewhere!"
-        });
-
-        // Simulates call to server
-        // Returns a promise, NOT items array directly
         service.getAllCategories = function () {
             let response = $http({
                 method: 'GET',
                 url: (ApiBasePath + '/categories.json')
             }).then(function (response) {
-                return response;
+                categories = response;
+                return categories;
             })
             .catch(function (err) {
                 return `Error: Unable to get items. ${err}`
@@ -47,7 +30,8 @@
                 method: 'GET',
                 url: (ApiBasePath + `/menu_items.json?category=${categoryShortName}`)
             }).then(function (response) {
-                return response;
+                items = response.menu_items ? response.menu_items : [];
+                return items;
             })
                 .catch(function (err) {
                     return `Error: Unable to get items. ${err}`
