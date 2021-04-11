2df4d3b9ef441daea01c6dd11410409fd8c8bdd9
Manuel Santiago <msanti16@jhu.edu>
2021-04-11 16:55:21 -0400

    removed MenuItem Controller

--- mod5_solution/src/public/menu-item/menu-item.component.js
+++ mod5_solution/src/public/menu-item/menu-item.component.js
@@ -6,22 +6,12 @@ angular.module('public')
   templateUrl: 'src/public/menu-item/menu-item.html',
   bindings: {
     menuItem: '<'
-  },
-  controller: MenuItemController
+  }
 })
 .filter('getCategory', function() {
   return function(text) {
     return text.replace(/[^a-zA-Z]+/g, '');
   }
 });
-
-
-MenuItemController.$inject = ['ApiPath'];
-function MenuItemController(ApiPath) {
-  var $ctrl = this;
-  $ctrl.basePath = ApiPath;
-  console.log("MenuItemController menuItem")
-  console.log($ctrl)
-}
 
 })();
