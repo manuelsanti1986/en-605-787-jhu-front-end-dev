1c40bc5ad947792b46666d2914937f6460581afa
Manuel Santiago <msanti16@jhu.edu>
2021-04-11 15:51:11 -0400

    Added styling to form

---
+++ mod5_solution/src/common/signup.service.js
@@ -0,0 +1,30 @@
+(function () {
+    "use strict";
+
+    angular.module('common')
+        .service('SignUpService', SignUpService);
+
+    SignUpService.$inject = [];
+    function SignUpService() {
+        let service = this;
+        let userInfo = {
+          first_name: "",
+          last_name: "",
+          email: "",
+          phone: ""
+        };
+
+        service.registerUser = function (user) {
+            service.userInfo = user;
+            console.log("service.userInfo")
+            console.log(service.userInfo)
+        };
+
+        service.getuserInfo = function () {
+            return userInfo;
+        };
+    }
+
+
+
+})();
--- mod5_solution/src/public/sign-up/sign-up.controller.js
+++ mod5_solution/src/public/sign-up/sign-up.controller.js
@@ -3,10 +3,15 @@
     angular.module('public')
         .controller('SignUpController', SignUpController);
 
-    function SignUpController() {
+    SignUpController.$inject = ['SignUpService'];
+    function SignUpController(SignUpService) {
         let registration = this;
         console.log("SignUpController")
+
         registration.submit = function () {
+            console.log("registration user ")
+            console.log(registration);
+            SignUpService.registerUser(registration.user);
             registration.completed = true;
         };
     }
