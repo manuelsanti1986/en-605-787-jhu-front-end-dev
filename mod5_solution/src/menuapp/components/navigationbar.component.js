b68a0ca68f9b375aee53afa4656fc41d4294f9e8
Manuel Santiago <msanti16@jhu.edu>
2021-04-11 12:36:20 -0400

    Using started code

--- mod5_solution/src/menuapp/components/navigationbar.component.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    'use strict';
-
-    let app = angular.module('DataApp');
-    app.component('navigationBar', {
-        templateUrl: 'src/menuapp/templates/navigation-bar.template.html',
-    });
-})();
